1863d5102e81199c757c25a7d3fb473ed971d2c7
Simone Baffelli <simone.baffelli@empa.ch>
2022-03-31 11:11:32 +0200

    Improved management of API secrets.

--- sensorutils/secrets.py
+++ sensorutils/secrets.py
@@ -2,14 +2,57 @@
 Module to manage secrets (API keys, etc)
 used to access different locations
 """
+import secrets
+from attr import dataclass
 import yaml
+from yaml import Loader
 from . import files as fu
+from typing import Optional, Dict, Union, List
+import pathlib as pl
+import itertools
+
+@dataclass
+class ServiceCredentials():
+    """
+    Class to represent one credential entry
+    in a secrets vault
+    """
+    name: str
+    secret: str
+    user: Optional[str] = None
+    
+@dataclass 
+class CredentialsVault():
+    """
+    Class to represent a vault storing
+    credentials
+    """
+    secrets: Dict[str, List[ServiceCredentials]]
+    path: Union[str, pl.Path]
+
+    @staticmethod
+    def from_file(file_path: Union[pl.Path, str]):
+        with open(file_path) as input:
+            config = yaml.load(input, Loader)
+        all_services = [d['service'] for d in config if 'service' in d.keys()]
+        secrets = {k:[ServiceCredentials(**el) for el in g] for k,g in itertools.groupby(all_services, lambda x: x['name'])}
+        return CredentialsVault(secrets=secrets, path=file_path)
+                
+    def get_secret(self, service: str, user:str=None) -> str:
+        try:
+            all_sec = self.secrets[service]
+        except KeyError:
+            raise KeyError(f"Service with name {service} does not exists in this vault: {self.path}")
+        if user:
+            try:
+                res = [s for s in all_sec if s.user==user][0]
+            except:
+                raise KeyError(f'User {user} for {service} does not exists in this vault: {self.path}')
+        else:
+            res = all_sec[0]
+        return res.secret
+        
 
 def get_key(service: str, user:str=None, file:str=f'/newhome/{fu.get_user()}/secrets.yml') -> str:
-    with open(file) as input:
-        config = yaml.load(input)
-    try:
-        pw = config[service]['key']
-    except KeyError as e:
-        raise KeyError(f"There is no key for the service '{service}' in '{file}'")
-    return pw
+    vault = CredentialsVault.from_file(file)
+    return vault.get_secret(service, user)
--- sensorutils/tests/test_secrets.py
+++ sensorutils/tests/test_secrets.py
@@ -10,10 +10,17 @@ class TestSecrets(unittest.TestCase):
 
     def setUp(self):
         self.k = 'AAB'
+        self.user = 'pippo'
+        self.k1 = 'pappo'
         self.secret = \
         f"""
-        decentlab:
-            key: {self.k}
+        - service:
+            name: decentlab
+            secret: {self.k}
+        - service:
+            name: other
+            user: {self.user}
+            secret: {self.k1}
         """
         with tf.NamedTemporaryFile(mode='w+t', delete=False) as cfg:
             cfg.writelines(self.secret)
@@ -25,8 +32,11 @@ class TestSecrets(unittest.TestCase):
         k = se.get_key('decentlab', file=self.path)
         self.assertEqual(k, self.k)
     
+    def test_read_user(self):
+        self.assertEqual(se.get_key('other', self.user, file=self.path), self.k1)
+
     def test_read_missing(self):
-        self.assertRaises(KeyError, se.get_key, 'other',file=self.path)
+        self.assertRaises(KeyError, se.get_key, 'obomp', file=self.path)
 
    
 if __name__ == '__main__':
