eb5f2e69dca0aed6d88a8c1264202d7398e61514
Simone Baffelli <simone.baffelli@empa.ch>
2022-04-07 11:14:05 +0200

    Added seutp file for sensorutils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='sensorutils',
+      version='0.0',
+      description='Utils to work with CO2 Sensors',
+      author='Simone Baffelli',
+      author_email='simone.baffelli@empa.ch',
+      packages=["sensorutils"]
+     )
