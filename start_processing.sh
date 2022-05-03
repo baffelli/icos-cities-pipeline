97065eb314ad26c56b17c5ece21de6c437975637
Simone Baffelli <simone.baffelli@empa.ch>
2022-05-03 15:28:11 +0200

    Improved entry script.

--- start_processing.sh
+++ start_processing.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 source  ~/.bash_profile
-LOG=/project/CarboSense/Software/LOGs/processing_$(date --iso-8601).log
+project_folder=/project/CarboSense/
+LOG=$project_folder/Software/LOGs/processing_$(date --iso-8601).log
+echo "The log files are in ${LOG}"
 echo "$(date  --rfc-3339='seconds') [INFO] Started processing at " >> $LOG
-cd /project/CarboSense/Software 
+cd $project_folder/Software 
 echo "$(date  --rfc-3339='seconds') [INFO] Moved into processing folder " >> $LOG
 eval "$(conda shell.bash hook)"
 echo "$(date  --rfc-3339='seconds') [INFO] Activating conda environment" >> $LOG
@@ -10,5 +12,5 @@ conda activate icos-cities
 echo "$(date  --rfc-3339='seconds') [INFO] Conda environment active" >> $LOG
 echo "$(date  --rfc-3339='seconds') [INFO] Conda environment used: $CONDA_PREFIX" >> $LOG
 echo "$(date  --rfc-3339='seconds') [INFO] Starting digdag" >> $LOG
-/newhome/basi/bin/digdag r process_carbosense.dig --session daily   1>> $LOG 2>&1 &
+/newhome/$(whoami)/bin/digdag r process_carbosense.dig --session daily   1>> $LOG 2>&1 &
 echo "$(date  --rfc-3339='seconds') [INFO] Starting digdag" >> $LOG& 
