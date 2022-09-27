2817e33d44e35b1cb3302d89950180e0b846de2f
Stuart Grange <stuart.grange@empa.ch>
2022-09-27 14:52:23 +0200

    :hammer: move a bunch of legacy R and SQL files to `legacy_*` directory & did my best not to move *.py files

--- MeteoSwissData4Carbosense/upload_meteo_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-conda activate carbosense
-Rscript /project/CarboSense/Software/MeteoSwissData4Carbosense/Import_DB_table_METEOSWISS_Measurements_sb.r
--- Statistical_Models/animation.sh
+++
@@ -1,3 +0,0 @@
-cd /project/muem/Covid-19_ZH/Results/TrafficCountMaps/
-
-mencoder 'mf://*.jpg' -mf w=992:h=960:fps=3:type=jpg -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:vbitrate=5580 -oac copy -o animation.avi
